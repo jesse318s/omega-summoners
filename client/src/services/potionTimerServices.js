d53d03b35487bf565ce256c39634e8868e613f19
jesse318s <jesse318s@gmail.com>
2022-08-09 18:31:05 -0500

    Added comments to services for dev/prod url

--- client/src/services/potionTimerServices.js
+++ client/src/services/potionTimerServices.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import Userfront from "@userfront/core";
+// api url varies between development and production
 const apiUrl = "http://localhost:8080/api/potionTimer";
 Userfront.init("rbvqd5nd");
 
